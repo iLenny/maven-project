b4d7e724f3e2e50c53bc3e3bd4c67cced8e4d733
Leibniz H. Berihuete <iLenny@users.noreply.github.com>
2018-11-03 14:24:32 -0500

    Fix test

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -21,8 +21,8 @@ public class TestGreeter {
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
 
-    // assertThat(greeter.greet(someone), containsString(someone));
-    assertTrue(false);
+    assertThat(greeter.greet(someone), containsString(someone));
+    //assertTrue(false);
   }
 
   @Test
