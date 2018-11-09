98cbae2a8339ed47fd7055098a8d83de61f26499
Leibniz H. Berihuete <iLenny@users.noreply.github.com>
2018-11-09 17:37:28 -0600

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -28,7 +28,7 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
-
+//
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
